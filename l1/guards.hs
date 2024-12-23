1f13ed90393cadc58b1143a71e714b4523db92b9
bkataru <baalateja.k@gmail.com>
2024-12-24 00:36:42 +0530

    checkpoint: guards

---
+++ l1/guards.hs
@@ -0,0 +1,13 @@
+-- GUARDS
+
+bookCategory :: Int -> String
+-- Guard clause is used for different page ranges
+bookCategory pages
+  | pages < 100 = "Short Story"
+  | pages < 300 = "Novel"
+  | pages < 1000 = "SK Book!"
+  | otherwise = "We don't know!"
+
+main = do
+  putStrLn "The category of your book is: "
+  print (bookCategory 100000)
